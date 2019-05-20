40a4ee678bffb44770cfc1959b3a32a0f6a2f035
taktheh <50790441+taktheh@users.noreply.github.com>
2019-05-20 23:30:28 +0900

    Change to use a div tag instead of iframe.

--- DMMdetail.user.js
+++ DMMdetail.user.js
@@ -5,6 +5,7 @@
 // @description  display a detail window on mouse over
 // @author       Takamaro the Hentai
 // @downloadURL  https://github.com/taktheh/DMMdetail/raw/master/DMMdetail.user.js
+// @match        https://www.dmm.com/*/list/*
 // @match        https://www.dmm.co.jp/*/list/*
 // @grant        none
 // ==/UserScript==
@@ -12,33 +13,53 @@
 (function() {
     'use strict';
 
-    const frameName = "DMMdetailFrame";
+    const idName = "DMMdetailFrame";
 
+    function load(url, func) {
+        var xhr = new XMLHttpRequest();
+        xhr.responseType = "document";
+        xhr.onreadystatechange = function() {
+            if ((xhr.readyState === 4) && (xhr.status === 200)) {
+                func(xhr.response)
+            }
+        }
+        xhr.open("GET", url, true);
+        xhr.send(null);
+    };
+    function dismiss(e) {
+    }
     function onmouseover() {
         var rect = this.getBoundingClientRect();
         var src = this.getElementsByTagName("a")[0].href;
         var timeoutID = window.setTimeout(() => {
-            var iframe = document.getElementById(frameName);
-            if (iframe.src === src) {
-                iframe.style.display = null;
-            }
-            else {
+            var popup = document.getElementById(idName);
+            load(src, (e) => {
+                var styles = e.getElementsByTagName("style");
+                for (var i = 0; i < styles.length; i++) {
+                    popup.appendChild(styles[i]);
+                }
+                var style = document.createElement("style");
+                popup.appendChild(style);
+                style.textContent =
+                    '.page-detail > table > tbody > tr > td:nth-child(n+2) ' +
+                    '{ display: none }';
+                popup.appendChild(e.getElementById("mu"));
                 var left = rect.left + window.pageXOffset;
                 var top = rect.top + window.pageYOffset;
-                iframe.src = src;
-                iframe.style.top = "" + top + "px";
+                popup.style.top = "" + top + "px";
                 if (left < window.innerWidth * 0.5) {
-                    iframe.style.left = "" + left + "px";
+                    popup.style.left = "" + left + "px";
                 } else {
-                    iframe.style.left = "" + window.innerWidth * 0.5 + "px";
+                    popup.style.left = "" + window.innerWidth * 0.5 + "px";
                 }
-                iframe.onload = function() {
-                    iframe.style.display = null;
-                    this.contentDocument.body.onmouseleave = function() {
-                        iframe.style.display = "none";
-                    };
+                popup.onmouseleave = function() {
+                    popup.style.display = "none";
+                    while (popup.firstChild) {
+                        popup.removeChild(popup.firstChild);
+                    }
                 };
-            }
+                popup.style.display = "block";
+            });
         }, 2500);
         this.onmouseout = function() {
             window.clearTimeout(timeoutID);
@@ -49,11 +70,12 @@
         var style = document.createElement("style");
         document.head.appendChild(style);
         style.sheet.insertRule(
-            '#' + frameName + ' {' +
+            '#' + idName + ' {' +
                 'position: absolute;' +
                 'z-index: 32767;' +
                 'width: 50vw;' +
                 'height: 50vh;' +
+                'overflow: scroll;' +
                 'background: #FFFFFF;' +
                 '}'
         );
@@ -62,10 +84,10 @@
     (function() {
         var list = document.getElementById('list');
         if (list) {
-            var iframe = document.createElement("iframe");
-            iframe.id = frameName;
-            iframe.style.display = "none";
-            document.body.appendChild(iframe);
+            var div = document.createElement("div");
+            div.id = idName;
+            div.style.display = "none";
+            document.body.appendChild(div);
 
             var tmb = list.getElementsByClassName('tmb');
             for (var i = 0; i < tmb.length; i++) {
