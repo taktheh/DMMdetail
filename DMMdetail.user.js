fd0282fc6fd532cbddbee6192aca740c3b5780ec
taktheh <50790441+taktheh@users.noreply.github.com>
2019-05-22 23:22:45 +0900

    Modify to recycle loaded elements.

--- DMMdetail.user.js
+++ DMMdetail.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DMMdetail
 // @namespace    https://github.com/taktheh/DMMdetail
-// @version      0.2
+// @version      0.3
 // @description  display a detail window on mouse over
 // @author       Takamaro the Hentai
 // @downloadURL  https://github.com/taktheh/DMMdetail/raw/master/DMMdetail.user.js
@@ -14,6 +14,7 @@
     'use strict';
 
     const idName = "DMMdetailFrame";
+    const wait = 2000;
 
     function load(url, func) {
         var xhr = new XMLHttpRequest();
@@ -26,41 +27,47 @@
         xhr.open("GET", url, true);
         xhr.send(null);
     };
-    function dismiss(e) {
+    function display_popup(popup, rect) {
+        var left = rect.left + window.pageXOffset;
+        var top = rect.top + window.pageYOffset;
+        popup.style.top = "" + top + "px";
+        if (left < window.innerWidth * 0.5) {
+            popup.style.left = "" + left + "px";
+        } else {
+            popup.style.left = "" + window.innerWidth * 0.5 + "px";
+        }
+        popup.onmouseleave = function() {
+            popup.style.display = "none";
+        };
+        popup.style.display = "block";
     }
     function onmouseover() {
         var rect = this.getBoundingClientRect();
         var src = this.getElementsByTagName("a")[0].href;
         var timeoutID = window.setTimeout(() => {
             var popup = document.getElementById(idName);
-            load(src, (e) => {
-                var styles = e.getElementsByTagName("style");
-                for (var i = 0; i < styles.length; i++) {
-                    popup.appendChild(styles[i]);
-                }
-                var style = document.createElement("style");
-                popup.appendChild(style);
-                style.textContent =
-                    '.page-detail > table > tbody > tr > td:nth-child(n+2) ' +
-                    '{ display: none }';
-                popup.appendChild(e.getElementById("mu"));
-                var left = rect.left + window.pageXOffset;
-                var top = rect.top + window.pageYOffset;
-                popup.style.top = "" + top + "px";
-                if (left < window.innerWidth * 0.5) {
-                    popup.style.left = "" + left + "px";
-                } else {
-                    popup.style.left = "" + window.innerWidth * 0.5 + "px";
-                }
-                popup.onmouseleave = function() {
-                    popup.style.display = "none";
-                    while (popup.firstChild) {
-                        popup.removeChild(popup.firstChild);
+            if (popup.src === src) {
+                display_popup(popup, rect);
+            } else {
+                load(src, (e) => {
+                    var styles = e.getElementsByTagName("style");
+                    for (var i = 0; i < styles.length; i++) {
+                        popup.appendChild(styles[i]);
                     }
-                };
-                popup.style.display = "block";
-            });
-        }, 2500);
+                    var style = document.createElement("style");
+                    popup.appendChild(style);
+                    style.textContent =
+                        '.page-detail>table>tbody>tr>td:nth-child(n+2)' +
+                        '{ display: none }';
+                    popup.appendChild(e.getElementById("mu"));
+                    display_popup(popup, rect);
+                });
+                while (popup.firstChild) {
+                    popup.removeChild(popup.firstChild);
+                }
+                popup.src = src;
+            }
+        }, wait);
         this.onmouseout = function() {
             window.clearTimeout(timeoutID);
         };
@@ -74,9 +81,11 @@
                 'position: absolute;' +
                 'z-index: 32767;' +
                 'width: 50vw;' +
-                'height: 50vh;' +
+                'height: 80vh;' +
                 'overflow: scroll;' +
                 'background: #FFFFFF;' +
+                'border: 1px #ccc solid;' +
+                'box-shadow: 2px 2px 2px 1px rgba(0,0,0,0.2);' +
                 '}'
         );
     })();
